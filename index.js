77b6b814ecb6f472bc62f711a2eb6335d83513ae
andreynho2006 <andreynho2006@gmail.com>
2019-06-29 18:42:21 +0300

    add redux and Provider to app

---
+++ index.js
@@ -0,0 +1,14 @@
+import React             from 'react';
+import ReactDOM          from 'react-dom';
+import App               from './components/app';
+import { Provider }      from 'react-redux';
+import { createStore }   from 'redux';
+
+const store = createStore(
+    () => {{}}
+);
+
+ReactDOM.render(
+    <Provider store={store}><App /></Provider>,
+    document.getElementById('app')
+)
