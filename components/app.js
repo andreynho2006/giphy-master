ac6a624decaa0f7852d8810b2d841d0b2c90b199
andreynho2006 <andreynho2006@gmail.com>
2019-06-30 08:49:58 +0300

    add a results object with 2 search results

--- components/app.js
+++ components/app.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import SearchForm from './search-form/search-form';
+import SearchResults from '../components/search-results/search-results';
+
+const results = [
+    {
+        full: "https://media1.giphy.com/media/g5KhmX06Q0XBu/100_s.gif?cid=0f230e2f5d184b366344516f733e3dec&rid=100_s.gif",
+        thumbnail: "https://media1.giphy.com/media/g5KhmX06Q0XBu/giphy_s.gif?cid=0f230e2f5d184b366344516f733e3dec&rid=giphy_s.gif",
+    },
+    {
+        full: "https://media2.giphy.com/media/uw3fTCTNMbXAk/100_s.gif?cid=0f230e2f5d184b366344516f733e3dec&rid=100_s.gif",
+        thumbnail: "https://media2.giphy.com/media/uw3fTCTNMbXAk/giphy_s.gif?cid=0f230e2f5d184b366344516f733e3dec&rid=giphy_s.gif",
+    }
+];
 
 export default () => {
     return (
-        <SearchForm onSearchSubmitted={(term) => console.log(term)} />
+        <div>
+            <SearchForm onSearchSubmitted={(term) => console.log(term)} />
+            <SearchResults results={results} />
+        </div>
     )
 };
--- components/search-results/search-results.js
+++ components/search-results/search-results.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as CustomTypes from '../../lib/custom-types';
+import SearcResult from '../search-result/search-result';
 
 export default function SearchResults ({ results }) {
     return (
         <div>
-
+            { results.map((result) => <SearcResult result={result} />) }
         </div>
     );
 }
